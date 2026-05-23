8394828e7cf0ca062bc3818a2617ebe10955eb18
EbenAdex <ebenadex3008@gmail.com>
2026-05-23 15:34:41 +0100

    add medals css

--- src/pages/Medals.jsx
+++ src/pages/Medals.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
 import { useAppData } from "../context/AppDataContext";
-import "../styles/Medals.css";
+import "../styles/medals.css";
 
 function Medals() {
   const { rankings = [] } = useAppData();
